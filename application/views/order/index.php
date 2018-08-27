f26c7d2745f254a0a7da4261bcd89045a5abd9ae
pgarcia <gasetogas123>
2018-08-27 09:49:54 +0200

    Orders select

--- application/views/order/index.php
+++ application/views/order/index.php
@@ -1,44 +1,43 @@
 
 
-    <div class="text-center">
+    <div class="pull-right">
         <button data-toggle="modal" data-target="#add_order_modal" class="btn btn-success">Novo</button>
-    </div><br>
-
-    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" >
-        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
-            <form id="users" method="post">
-                <div class="form-group">
-                            <label for="id_destination" class="control-label">Users</label>
-
-                            <!-- Select with all the users.-->
-                            <select class="form-control" id="id_user" name="id_user">
-                                <?php foreach($users as $u){ ?>
-                                    <option value="<?=$u['id']?>"><?=$u['name']?></option>
-                                <?php } ?>
-                            </select>
-                </div>
-                <button id="orderId" type="button" class="btn btn-primary btn-block">Submit</button>
-            </form>
-        </div>
-        <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
-            <table class="table table-striped table-bordered">
-                <tr>
-                    <th>ID</th>
-                    <th>Date</th>
-                    <th>Reason</th>
-                    <th>Destination</th>
-                    <th>Id User</th>
-                </tr>
-                <?php foreach($orders as $o){ ?>
-                <tr>
-                    <td><?php echo $o['id']; ?></td>
-                    <td><?php echo $o['date']; ?></td>
-                    <td><?php echo $o['reason']; ?></td>
-                    <td><?php echo $o['id_destination']; ?></td>
-                    <td><?php echo $o['id_user']; ?></td>
-                </tr>
-                <?php } ?>
-            </table>
-        </div>
     </div>
+
+    <form id="users" method="post">
+        <div class="form-group">
+
+                <div class="col-xl-12 form-group">
+                    <label for="id_destination" class="col-md-4 control-label">Destination</label>
+
+                    <!-- Select with all the users.-->
+                    <select class="form-control" id="id_user" name="id_user">
+                        <?php foreach($users as $u){ ?>
+                            <option value="<?=$u['id']?>"><?=$u['name']?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+<table class="table table-striped table-bordered">
+    <tr>
+		<th>ID</th>
+		<th>Date</th>
+		<th>Reason</th>
+		<th>Destination</th>
+		<th>Id User</th>
+    </tr>
+	<?php foreach($orders as $o){ ?>
+    <tr>
+		<td><?php echo $o['id']; ?></td>
+		<td><?php echo $o['date']; ?></td>
+		<td><?php echo $o['reason']; ?></td>
+		<td><?php echo $o['id_destination']; ?></td>
+		<td><?php echo $o['id_user']; ?></td>
+    </tr>
+	<?php } ?>
+</table>
+
 <?php echo isset($add_modal) ? $add_modal : '' ; ?>
